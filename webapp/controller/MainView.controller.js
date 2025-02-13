e8f16bc0eb3978cef84f6753c7d08b8aa37be3ca
Martindls <martindls2107@gmail.com>
2025-02-13 22:43:13 +0000

    Borrar Filtros implementado

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
+], (BaseController) => {
     "use strict";
 
-    return Controller.extend("martindls.invoices.controller.MainView", {
+    return BaseController.extend("martindls.invoices.controller.MainView", {
         onInit() {
+            const oJSONModel = new sap.ui.model.json.JSONModel();
+            const oView = this.getView();
+            oJSONModel.loadData("/model/selectionScreenMenu.json");
+            oView.setModel(oJSONModel, "selectionScreenMenu");
+        },
+
+
+        onFilter(oEvent) {
+
+        },
+        
+
+        onClearFilter() {
+            const oModelSelScreen = this.getView().getModel("selectionScreenMenu");
+            oModelSelScreen.setProperty("/ShipName", "");
+            oModelSelScreen.setProperty("/CountryKey", "");
         }
     });
 });
